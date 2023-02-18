f809b96950d2e5ddc52f55c925e525bf8fb113a3
chchiu <chchiu@pass-2.ece.utah.edu>
2023-02-18 10:19:54 -0700

    google benchmark cmake

---
+++ assignment_1/cmake/benchmark.cmake
@@ -0,0 +1,30 @@
+include(ExternalProject)
+
+#set(GB_ROOT         ${CMAKE_BINARY_DIR}/benchmark)
+#set(GB_LIB_DIR      ${GB_ROOT}/lib)
+#set(GB_INCLUDE_DIR  ${GB_ROOT}/include)
+
+set(GB_URL          ${CMAKE_SOURCE_DIR}/3rd-party/benchmark.tar.gz)
+set(GB_CONFIGURE    cd ${CMAKE_BINARY_DIR}/benchmark &&
+                    mkdir build && 
+                    cd ${CMAKE_BINARY_DIR}/benchmark/build &&
+                    cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/benchmark/build ../)
+set(GB_MAKE         cd ${CMAKE_BINARY_DIR}/benchmark/build && make -j8)
+set(GB_INSTALL      cd ${CMAKE_BINARY_DIR}/benchmark/build && make install)
+
+#message(STATUS "CMAKE SOURCE DIR ${CMAKE_SOURCE_DIR}")
+#message(STATUS "CMAKE BINARY DIR ${CMAKE_BINARY_DIR}")
+#message(STATUS "GB ROOT ${GB_ROOT}")
+
+ExternalProject_Add(benchmark
+  URL                ${GB_URL}
+  SOURCE_DIR         ${CMAKE_BINARY_DIR}/benchmark
+#  PREFIX             ${CMAKE_BINARY_DIR}/benchmark
+  CONFIGURE_COMMAND  ${GB_CONFIGURE}
+  BUILD_COMMAND      ${GB_MAKE}
+  INSTALL_COMMAND    ${GB_INSTALL}
+)
+
+add_library(gbenchmark STATIC IMPORTED)
+set_property(TARGET gbenchmark PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/benchmark/build/lib64/libbenchmark.a)
+add_dependencies(gbenchmark benchmark)
